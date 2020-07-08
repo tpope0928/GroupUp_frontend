6db9de79114d86f04306987b4653175822922b6a
tpope0928 <tpope0928@gmail.com>
2020-07-08 19:02:53 -0400

    7/8/2020 stopping point

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log('in index.js');
-console.log('another test')
+const endpoint = "http://localhost:3000/api/v1/games"
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            console.log();
+        })
+})
+
