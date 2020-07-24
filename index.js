ff7c91b8d0f864360eb970398439cf74c3daa8a2
tpope0928 <tpope0928@gmail.com>
2020-07-23 21:23:54 -0400

    end of day

--- index.js
+++ index.js
@@ -8,21 +8,8 @@ function getPlayerData() {
     fetch(endPoint)
         .then(response => response.json())
         .then(playerData => {
-            playerData.data.forEach(player => {
-                // double check how your data is nested in the console so you can successfully access the attributes of each individual object
-                const playerMarkup = `
-                    <div data-id=${player.id}>
-                        <h3>${player.attributes.username} - ${player.attributes.city}, ${player.attributes.state}</h3>
-                        <p>Game: ${player.attributes.title}</p>
-                        <p>Genre: ${player.attributes.genre}</p>
-                        <p>Game Level: ${player.attributes.level}</p>
-                        <p>Competitive: ${player.attributes.competitive}</p>
-                        <button data-id=${player.id}>Group Up!</button>
-                    </div>
-                    <br><br>`;
+            console.log(playerData)
 
-                document.querySelector('#player-container').innerHTML += playerMarkup
-            })
         })
 }
 
