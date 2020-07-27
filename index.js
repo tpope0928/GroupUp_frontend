17755fb05349b9876ef4952fca386f9bb2db9f34
tpope0928 <tpope0928@gmail.com>
2020-07-26 20:14:38 -0400

    nested data trouble

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const endPoint = "http://localhost:3000/api/v1/players"
 
 document.addEventListener('DOMContentLoaded', () => {
-    getPlayerData()
-})
+    getPlayers()
+});
 
-function getPlayerData() {
+function getPlayers() {
     fetch(endPoint)
         .then(response => response.json())
-        .then(playerData => {
-            console.log(playerData)
-
+        .then(players => {
+            console.log(players);
+            players.data.forEach(player => {
+                const playerMarkup = `
+                    <div data-id=${player.id}>
+                    <h3>${player.attributes.username} - ${player.attributes.city}, ${player.attributes.state}</h3>
+                        <p>Game: ${player.attributes.games.title}</p>
+                        <p>Genre: ${player.attributes.games.genre}</p>
+                        <p>Game Level: ${player.attributes.games.level}</p>
+                        <p>Competitive: ${player.attributes.games.competitive}</p>
+                        <button data-id=${player.id}>Group Up!</button>
+                    </div>
+                    <br><br>`;
+                document.querySelector('#player-container').innerHTML += playerMarkup
+            })
         })
 }
-
